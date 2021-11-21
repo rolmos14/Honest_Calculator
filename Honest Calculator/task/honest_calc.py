926fa8e259e62e2b6e69cc636fefdbdc1d21b620
rolmos14 <olmos.rafa@gmail.com>
2021-11-21 22:07:27 +0100

    Stage 1/5. Most relevant code is how to check that x and y are integers or float numbers.

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,1 +1,17 @@
-# write your code here
+messages = {"msg_0": "Enter an equation",
+            "msg_1": "Do you even know what numbers are? Stay focused!",
+            "msg_2": "Yes ... an interesting math operation. You've slept through all classes, haven't you?"}
+
+while True:
+    print(messages["msg_0"])
+    x, oper, y = input().split()
+    if (x.isdigit() or x.replace(".", "", 1).isdigit() and x.count(".") < 2) and \
+            (y.isdigit() or y.replace(".", "", 1).isdigit() and y.count(".") < 2):
+        pass
+    else:
+        print(messages["msg_1"])
+        continue
+    if oper not in ["+", "-", "*", "/"]:
+        print(messages["msg_2"])
+        continue
+    break
