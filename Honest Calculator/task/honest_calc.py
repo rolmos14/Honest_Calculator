b482bdb6cdd861fae40c60decda9f216bd35fb7d
rolmos14 <olmos.rafa@gmail.com>
2021-11-23 22:27:16 +0100

    Stage 2/5. Flowchart is now completed with the basic operations. Dictionary keys are simplified, we don't need strings as keys.

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,19 +1,19 @@
-messages = {"msg_0": "Enter an equation",
-            "msg_1": "Do you even know what numbers are? Stay focused!",
-            "msg_2": "Yes ... an interesting math operation. You've slept through all classes, haven't you?",
-            "msg_3": "Yeah... division by zero. Smart move..."}
+messages = {0: "Enter an equation",
+            1: "Do you even know what numbers are? Stay focused!",
+            2: "Yes ... an interesting math operation. You've slept through all classes, haven't you?",
+            3: "Yeah... division by zero. Smart move..."}
 
 while True:
-    print(messages["msg_0"])
+    print(messages[0])
     x, oper, y = input().split()
     if (x.isdigit() or x.replace(".", "", 1).isdigit() and x.count(".") < 2) and \
             (y.isdigit() or y.replace(".", "", 1).isdigit() and y.count(".") < 2):
         pass
     else:
-        print(messages["msg_1"])
+        print(messages[1])
         continue
     if oper not in ["+", "-", "*", "/"]:
-        print(messages["msg_2"])
+        print(messages[2])
         continue
     if oper == "+":
         result = float(x) + float(y)
@@ -27,6 +27,6 @@ while True:
     if oper == "/" and float(y):
         result = float(x) / float(y)
         break
-    print(messages["msg_3"])
+    print(messages[3])
 
 print(result)
